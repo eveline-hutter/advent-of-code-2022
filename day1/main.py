0dad0ca079f0f105d165caa172586a357c9776c0
eveline-hutter <eveline.hutter@students.ffhs.ch>
2022-12-01 06:47:30 +0100

    solved day1

--- day1/main.py
+++ day1/main.py
@@ -1,14 +1,26 @@
 with open('input1.txt') as file:
-    input = [int(line) for line in file]
+    input = file.read().splitlines()  # open without /n
 
 
 def puzzle1():
-    print()
+    elves_calories = []
+    calories = 0
+
+    for cal in input:
+        if cal == '':
+            elves_calories.append(calories)
+            calories = 0
+        else:
+            calories += int(cal)
+
+    return elves_calories
 
 
-def puzzle2():
-    print()
+def puzzle2(elves_calories):
+    sorted_elves_calories = sorted(elves_calories, reverse=True)
+    print('puzzle2:', sum(sorted_elves_calories[:3]))
 
 
-puzzle1()
-puzzle2()
+elves_calories = puzzle1()
+print('puzzle1:', max(elves_calories))
+puzzle2(elves_calories)
